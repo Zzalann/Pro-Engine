5cb63e8c6dbc420def6f1db4b7ace8f9a541b5ab
Zzalann <120551018+Zzalann@users.noreply.github.com>
2024-10-28 17:20:39 +0100

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -59,13 +59,13 @@ gsap.fromTo(".js-gepsz1",
   );
 
   gsap.fromTo(".js-gepsz2", 
-    { x: 2000,
+    { y: 2000,
       opacity: 0
     }, 
     { 
       delay: 1.4,
       opacity: 1,
-      x: 0, 
+      y: 0, 
       duration: 2, 
       ease: "power2.out",
       scrollTrigger: {
@@ -75,12 +75,12 @@ gsap.fromTo(".js-gepsz1",
     }
   );
   gsap.fromTo(".js-gepk2", 
-    { x: 2000,
+    { y: 2000,
       opacity: 0
     }, 
     { 
       delay: 1.2,
-      x: 0, 
+      y: 0, 
       opacity: 1,
       duration: 2, 
       ease: "power2.out",
